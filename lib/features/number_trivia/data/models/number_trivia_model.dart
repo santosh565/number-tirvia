54baf8d334a49d78d8370e4f192786f03a7f8bad
Santosh Bhattarai <hsotnas76@gmail.com>
2021-08-22 17:37:50 +0545

    adds number trivia remote data source

--- lib/features/number_trivia/data/models/number_trivia_model.dart
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -1,4 +1,4 @@
-import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+import '../../domain/entities/number_trivia.dart';
 import 'package:meta/meta.dart';
 
 class NumberTriviaModel extends NumberTrivia {
